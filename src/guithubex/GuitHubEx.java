d643e410ae20d68d3e3fcaf712c80b3e4da90533
usuario <usuario@DESKTOP-PKM1OVC>
2020-04-20 20:00:17 -0500

    Calculos estadísticos

---
+++ src/guithubex/GuitHubEx.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guithubex;
+
+/**
+ *
+ * @author usuario
+ */
+public class GuitHubEx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI interfaz = new GUI();
+        interfaz.setVisible(true);
+    }
+    
+}
